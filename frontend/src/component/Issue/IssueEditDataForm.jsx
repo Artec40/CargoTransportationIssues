d26da4490b9b765235d5eed1b6d818840b8a65e3
dnwdark <Artec40@gmail.com>
2021-03-18 23:53:41 +0500

    Корректировка работы формы совместно с TextField.

--- frontend/src/component/Issue/IssueEditDataForm.jsx
+++ frontend/src/component/Issue/IssueEditDataForm.jsx
@@ -4,28 +4,40 @@ import { Field, reduxForm } from 'redux-form'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import SaveIcon from '@material-ui/icons/Save'
-//todo настроить форму, работающую с materiaLUI
-const IssueEditDataForm = ({handleSubmit, initialValues}) => {
+
+const renderTextField = (
+    { input, label, meta: { touched, error }, ...custom },
+) => (
+    <TextField color={'secondary'}
+               hintText={label}
+               floatingLabelText={label}
+               {...input}
+               {...custom}
+    />
+)
+
+const IssueEditDataForm = ({handleSubmit}) => {
     return (
         <form className={s.Issue} onSubmit={handleSubmit}>
-            <TextField className={s.value}
-                       color={'secondary'}
-                       label={'Дата и время получения заявки от клиента'}
-                       value={initialValues? initialValues.date : ''}/>
-            <Field className={s.value} name={'date'} component={'input'}/>
+            <div className={s.description}>Дата и время получения заявки от клиента:</div>
+            <Field className={s.value} name={'date'} component={renderTextField}/>
             <div className={s.description}>Название фирмы клиента:</div>
-            <Field className={s.value} name={'company'} component={'input'}/>
+            <Field className={s.value} name={'company'} component={renderTextField}/>
             <div className={s.description}>ФИО перевозчика:</div>
-            <Field className={s.value} name={'carrier'} component={'input'}/>
+            <Field className={s.value} name={'carrier'} component={renderTextField}/>
             <div className={s.description}>Контактный телефон перевозчика:</div>
-            <Field className={s.value} name={'phone'} component={'input'}/>
+            <Field className={s.value} name={'phone'} component={renderTextField}/>
             <div className={s.description}>Коментарии:</div>
-            <Field className={s.value} name={'comments'} component={'input'}/>
+            <Field className={s.value} name={'comments'} component={renderTextField}/>
             <div className={s.description}>ATI код перевозчика:</div>
-            <Field className={s.value} name={'ATICode'} component={'input'}/>
+            <Field className={s.value} name={'ATICode'} component={renderTextField}/>
             <div>
-                <button>sd</button>
-                <Button endIcon={<SaveIcon/>} variant={'contained'} size={'small'}>Сохранить заявку</Button>
+                <Button type={'submit'}
+                        endIcon={<SaveIcon/>}
+                        variant={'contained'}
+                        size={'small'}>
+                    Сохранить заявку
+                </Button>
             </div>
         </form>
     )
